7ea89515ebc2d2fa807ec49bd3efed4c0219cb93
root <root@ldap.(none)>
2018-05-13 05:49:23 +0800

    fishied openldap

--- install_openldap/install.sh
+++ install_openldap/install.sh
@@ -26,8 +26,41 @@ echo $pass_tmp >> tmpinfo
 log_info "copy the slapd.confi file"
 cp /usr/share/openldap-servers/slapd.conf.obsolete  /etc/openldap/slapd.conf
 sed -i "/^suffix/s/dc=my-domain,dc=com/$cfg_suffix/" /etc/openldap/slapd.conf
-sed -i "/^rootdn/s/dc=my-domain,dc=com/$cfg_suffix/" /etc/openldap/slapd.conf
+sed -i "/^rootdn/s/cn=Manager,dc=my-domain,dc=com/cn=root,$cfg_suffix/" /etc/openldap/slapd.conf
 sed -i "/^\# rootpw\s*secret$/arootpw\t\t$root_pass_tmp" /etc/openldap/slapd.conf
 cp /usr/share/openldap-servers/DB_CONFIG.example /var/lib/ldap/DB_CONFIG
 chown -R ldap:ldap /etc/openldap
 chown -R ldap:ldap /var/lib/ldap
+log_info "first start the slapd"
+/etc/init.d/slapd restart
+
+
+log_info "recreate the config file \"slaptest -f /etc/openldap/slapd.conf -F /etc/openldap/slapd.d/\""
+rm -rf /etc/openldap/slapd.d/*
+slaptest -f /etc/openldap/slapd.conf -F /etc/openldap/slapd.d/
+chown -R ldap.ldap /etc/openldap/slapd.d/
+/etc/init.d/slapd restart
+cat > root.ldif <<EOF
+dn: $cfg_suffix
+objectclass: dcObject
+objectclass: organization
+o: Shichun,Inc.
+dc: linshichun
+
+dn: cn=root,$cfg_suffix
+objectclass: organizationalRole
+cn: root
+EOF
+
+ldapadd -x -D "cn=root,$cfg_suffix" -w 111111 -f root.ldif
+
+log_info "============================================================"
+log_info "start to install phpldapadmin"
+rpm -ivh  http://mirrors.ukfast.co.uk/sites/dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm        
+yum clean all 
+yum makecache
+yum install -y phpldapadmin
+sed -i "/Allow from ::1/aAllow from 192.168.1.100" /etc/httpd/conf.d/phpldapadmin.conf
+
+sed -i "s/^\$servers->setValue('login','attr','uid')/\/\/&/" /etc/phpldapadmin/config.php
+/etc/init.d/httpd restart
--- install_openldap/uninstall.sh
+++ install_openldap/uninstall.sh
@@ -8,6 +8,12 @@ else
     echo -e "./log_function.sh is not exist." 
     exit 1
 fi
+log_info "stop slap"
+/etc/init.d/slapd stop
+log_info "uninstall phpldapadmin"
+rpm -e --nodeps phpldapadmin
+rm -rf /etc/phpldapadmin
+rm -rf /etc/httpd/conf.d/phpldapadmin.conf.rpmsave
 rpms=`rpm -qa |grep "ldap-[^0-9]"`
 for rpmk in $rpms
 do
@@ -18,4 +24,5 @@ log_info "uninstall migrationtools"
 rpm -e --nodeps migrationtools
 log_info "start to delete config file."
 rm -rf /etc/openldap
-rm -rf /var/lib/
+rm -rf /var/lib/ldap
+
