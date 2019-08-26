397ee502022255d2fc5f520cb53d4673e9c9905e
linshichun <linshichun19910510@126.com>
2019-08-26 23:51:32 +0800

    Update install.sh
    新版本

--- install_python3/install.sh
+++ install_python3/install.sh
@@ -1,13 +1,39 @@
 #!/bin/bash
-yum -y install zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-devel gcc
-if [ ! -f Python-3.6.5.tar.xz ]; then
-wget https://www.python.org/ftp/python/3.6.5/Python-3.6.5.tar.xz
+# yum -y install libffi-devel wget vim zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-devel gcc
+home_path=$(cd "$(dirname "~/")";pwd)
+install_path=${home_path}/soft/python3
+# 下载
+if [ ! -f Python-3.7.4.tar.xz ]; then
+wget https://www.python.org/ftp/python/3.7.4/Python-3.7.4.tar.xz
 fi
-rm -rf Python-3.6.5
-tar xvJf  Python-3.6.5.tar.xz
-mkdir /usr/local/python3
-cd Python-3.6.5
-./configure --prefix=/usr/local/python3
-make 
-make install > python_install.log
 
+# 解压
+rm -rf Python-3.7.4
+tar xvJf  Python-3.7.4.tar.xz
+
+# 创建目录
+if [ -d ${install_path} ]; then
+rm -rf ${install_path}
+fi
+mkdir -p ${install_path}
+
+# 安装
+cd Python-3.7.4
+./configure --prefix=${install_path}
+make 
+make install 
+
+# 软连接
+if [ -d ${install_path}/bin/pip3  ]; then
+rm -rf ${install_path}/bin/pip3
+fi
+
+if [ -d ${install_path}/bin/pip3  ]; then
+rm -rf ${install_path}/bin/python3
+fi
+
+if [ ! -d ${home_path}/bin/  ]; then
+mkdir -p ${home_path}/bin/
+fi
+ln -s ${install_path}/bin/pip3 ${home_path}/bin/pip 
+ln -s ${install_path}/bin/python3 ${home_path}/bin/python3 
